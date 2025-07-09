fc3ee55524de4613b865b85f03129f1f9f893401
Phuc Hung <nphung.htkhang2020@gmail.com>
2025-07-09 22:05:15 +0700

    add profile

--- lib/profile/my_profile.dart
+++ lib/profile/my_profile.dart
@@ -1,3 +1,4 @@
+import 'package:begining/screen/navigation.dart';
 import 'package:flutter/material.dart';
 
 class MyProfile extends StatelessWidget {
@@ -5,10 +6,269 @@ class MyProfile extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('My Profile'),
-        centerTitle: true,
+      body: Padding(
+        padding: const EdgeInsets.all(12.0),
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const SizedBox(height: 40),
+              const Text(
+                'Settings',
+                style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+              ),
+              const SizedBox(height: 10),
+              const Text(
+                'Personal',
+                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+              ),
+              const SizedBox(height: 30),
+              Container(
+                decoration: BoxDecoration(
+                  border: Border(
+                    bottom: BorderSide(width: 0.5, color: Colors.grey),
+                  ),
+                ),
+                child: TextButton(
+                  onPressed: () => {
+                    // Navigator.push(
+                    //   context,
+                    //   MaterialPageRoute(builder: (context) => const HomeScreen()),
+                    // ),
+                  },
+                  style: ButtonStyle(
+                    backgroundColor: WidgetStatePropertyAll(Colors.white),
+                    foregroundColor: WidgetStatePropertyAll(Colors.black),
+                    shadowColor: WidgetStatePropertyAll(
+                      Colors.black.withOpacity(0.5),
+                    ),
+                    shape: WidgetStatePropertyAll(
+                      RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(40),
+                        side: BorderSide(color: Colors.transparent, width: 1),
+                      ),
+                    ),
+                    minimumSize: WidgetStatePropertyAll(
+                      const Size(double.infinity, 50),
+                    ),
+                  ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        'Profile',
+                        style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.w400,
+                        ),
+                      ),
+                      Icon(Icons.arrow_forward_ios, color: Colors.black),
+                    ],
+                  ),
+                ),
+              ),
+              const SizedBox(height: 10),
+              Container(
+                decoration: BoxDecoration(
+                  border: Border(
+                    bottom: BorderSide(width: 0.5, color: Colors.grey),
+                  ),
+                ),
+                child: TextButton(
+                  onPressed: () => {
+                    // Navigator.push(
+                    //   context,
+                    //   MaterialPageRoute(builder: (context) => const HomeScreen()),
+                    // ),
+                  },
+                  style: ButtonStyle(
+                    backgroundColor: WidgetStatePropertyAll(Colors.white),
+                    foregroundColor: WidgetStatePropertyAll(Colors.black),
+                    shadowColor: WidgetStatePropertyAll(
+                      Colors.black.withOpacity(0.5),
+                    ),
+                    shape: WidgetStatePropertyAll(
+                      RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(40),
+                        side: BorderSide(color: Colors.transparent, width: 1),
+                      ),
+                    ),
+                    minimumSize: WidgetStatePropertyAll(
+                      const Size(double.infinity, 50),
+                    ),
+                  ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        'Your Orders',
+                        style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.w400,
+                        ),
+                      ),
+                      Icon(Icons.arrow_forward_ios, color: Colors.black),
+                    ],
+                  ),
+                ),
+              ),
+              const SizedBox(height: 10),
+              Container(
+                decoration: BoxDecoration(
+                  border: Border(
+                    bottom: BorderSide(width: 0.5, color: Colors.grey),
+                  ),
+                ),
+                child: TextButton(
+                  onPressed: () => {
+                    // Navigator.push(
+                    //   context,
+                    //   MaterialPageRoute(builder: (context) => const HomeScreen()),
+                    // ),
+                  },
+                  style: ButtonStyle(
+                    backgroundColor: WidgetStatePropertyAll(Colors.white),
+                    foregroundColor: WidgetStatePropertyAll(Colors.black),
+                    shadowColor: WidgetStatePropertyAll(
+                      Colors.black.withOpacity(0.5),
+                    ),
+                    shape: WidgetStatePropertyAll(
+                      RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(40),
+                        side: BorderSide(color: Colors.transparent, width: 1),
+                      ),
+                    ),
+                    minimumSize: WidgetStatePropertyAll(
+                      const Size(double.infinity, 50),
+                    ),
+                  ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        'Your Cart',
+                        style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.w400,
+                        ),
+                      ),
+                      Icon(Icons.arrow_forward_ios, color: Colors.black),
+                    ],
+                  ),
+                ),
+              ),
+              const SizedBox(height: 10),
+              Container(
+                decoration: BoxDecoration(
+                  border: Border(
+                    bottom: BorderSide(width: 0.5, color: Colors.grey),
+                  ),
+                ),
+                child: TextButton(
+                  onPressed: () => {
+                    // Navigator.push(
+                    //   context,
+                    //   MaterialPageRoute(builder: (context) => const HomeScreen()),
+                    // ),
+                  },
+                  style: ButtonStyle(
+                    backgroundColor: WidgetStatePropertyAll(Colors.white),
+                    foregroundColor: WidgetStatePropertyAll(Colors.black),
+                    shadowColor: WidgetStatePropertyAll(
+                      Colors.black.withOpacity(0.5),
+                    ),
+                    shape: WidgetStatePropertyAll(
+                      RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(40),
+                        side: BorderSide(color: Colors.transparent, width: 1),
+                      ),
+                    ),
+                    minimumSize: WidgetStatePropertyAll(
+                      const Size(double.infinity, 50),
+                    ),
+                  ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        'Shipping Address',
+                        style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.w400,
+                        ),
+                      ),
+                      Icon(Icons.arrow_forward_ios, color: Colors.black),
+                    ],
+                  ),
+                ),
+              ),
+              const SizedBox(height: 40),
+              Text(
+                'Personal',
+                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+              ),
+              Container(
+                decoration: BoxDecoration(
+                  border: Border(
+                    bottom: BorderSide(width: 0.5, color: Colors.grey),
+                  ),
+                ),
+                child: TextButton(
+                  onPressed: () => {
+                    // Navigator.push(
+                    //   context,
+                    //   MaterialPageRoute(builder: (context) => const HomeScreen()),
+                    // ),
+                  },
+                  style: ButtonStyle(
+                    backgroundColor: WidgetStatePropertyAll(Colors.white),
+                    foregroundColor: WidgetStatePropertyAll(Colors.black),
+                    shadowColor: WidgetStatePropertyAll(
+                      Colors.black.withOpacity(0.5),
+                    ),
+                    shape: WidgetStatePropertyAll(
+                      RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(40),
+                        side: BorderSide(color: Colors.transparent, width: 1),
+                      ),
+                    ),
+                    minimumSize: WidgetStatePropertyAll(
+                      const Size(double.infinity, 50),
+                    ),
+                  ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        'Logout',
+                        style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.w400,
+                        ),
+                      ),
+                      Icon(Icons.arrow_forward_ios, color: Colors.black),
+                    ],
+                  ),
+                ),
+              ),
+              const SizedBox(height: 20),
+              Text('Richard Shop', 
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Text(
+                'Version 1.0.0',
+                style: TextStyle(
+                  fontSize: 13,
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
+      bottomNavigationBar: Navigation(),
     );
   }
 }
